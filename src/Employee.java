ae8ef15f5ebd4b5ce3f71d7121a894cc32b02018
pisksand <pisksand@kkg-do.eu>
2022-09-06 11:25:03 +0200

    aaaa

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,18 @@
 public class Employee {
+    private String name;
+    private String vorname;
+    private String geburtsdatum;
+    private char geschlecht;
+    private int alter;
+    private double gewicht;
+    private String abteilung;
+    private double gehalt;
+    private int dienstjahre;
+    private String besonderheiten;
+
+    public Employee(int dienstjahre, double gehalt, String abteilung) {
+        this.dienstjahre = dienstjahre;
+        this.gehalt = gehalt;
+        this.abteilung = abteilung;
+    }
 }
--- src/MainProgramm.java
+++ src/MainProgramm.java
@@ -1,3 +1,7 @@
 public class MainProgramm {
-
+    public static void main(String[] args) {
+        Employee p1 = new Employee(0,1500,"A38");
+        Employee p2 = new Employee(3, 1800,"A1");
+        Employee p3 = new Employee(40, 5000, "F36");
+    }
 }
